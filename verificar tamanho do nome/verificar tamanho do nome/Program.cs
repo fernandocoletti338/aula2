59458428e7ccbb878b7993317d8335ed1baba8ad
Fernando Coletti <nandocoletti@hotmail.com>
2019-09-11 14:47:23 -0300

    calculo nome

---
+++ verificar tamanho do nome/verificar tamanho do nome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificar_tamanho_do_nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome a ser calculado");
+            var nomeLength = Console.ReadLine();
+
+            Console.WriteLine($"O tamanho deste nome é: {nomeLength.Replace(" ", "").Length}");
+            Console.ReadKey();
+        }
+    }
+}
