a94b5e4a998575f21628c7612412bf3a73679209
Fernando Coletti <nandocoletti@hotmail.com>
2019-09-12 17:23:57 -0300

    for and while

---
+++ LacosDeRepeticao/UtilizandoFor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nome = "Meu nome é Fernando Coletti e meu nome era pra ser Ferdinando";
+            var countFind = 0;
+            Console.WriteLine("Encontre uma palavra:");
+            var palavra = Console.ReadLine();
+
+
+            for (int i = 0; i < (nome.Length -2); i++)
+            {
+                var palavraParaComparar = nome[i].ToString() +
+                    nome[i + 1].ToString() +
+                    nome[i + 2].ToString();
+
+                if (palavra == palavraParaComparar) countFind++;
+
+            }
+
+            Console.WriteLine($"quantidade total de '{palavra}' {countFind}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LacosDeRepeticao/utilizandoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+
+            while (count < 1000000)
+            {
+                Console.WriteLine($"Numero:{count}");
+                count++;
+            }
+            Console.WriteLine("terminou o loop");
+            Console.ReadKey();
+        }
+    }
+}
