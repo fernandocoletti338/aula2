3b55e86498dff918234290bc9461e907edc10338
Fernando Coletti <nandocoletti@hotmail.com>
2019-10-02 08:19:05 -0300

    save

---
+++ 30-09-2019_04-10-2019/SQLQuery3.sql
@@ -0,0 +1,7 @@
+﻿select
+car.Modelo, --coluna da nossa tabela carros modelo
+mar.Nome, --coluna da nossa tabela marcas nome
+car.Placa, --coluna da nossa tabela carros placa
+car.Ano --coluna da nossa tabela carros ano
+from--indicador que vamos selecionar 
+Carros car inner join Marcas mar on car.MarcaCodigo = mar.Codigo
