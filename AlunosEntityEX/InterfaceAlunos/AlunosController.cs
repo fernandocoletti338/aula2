78f5faf8d9ce4fcc80f2f61be9b501f240e1de9f
Fernando Coletti <nandocoletti@hotmail.com>
2019-10-07 21:28:06 -0300

    não consegui fazer

---
+++ AlunosEntityEX/InterfaceAlunos/AlunosController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceAlunos
+{
+    internal class AlunoController
+    {
+        internal object GetAlunos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlunosEntityEX/InterfaceAlunos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityClassEX.Model;
+using EntityClassEX.Controller;
+
+namespace InterfaceAlunos
+{
+    class Program
+    {
+        static AlunoController alunos = new AlunoController();
+        static void Main(string[] args)
+        {
+            alunos.GetAlunos().T
+        }
+    }
+}
