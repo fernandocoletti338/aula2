824faad23561537c3a972dae1196a81e6dc13f04
Fernando Coletti <nandocoletti@hotmail.com>
2019-10-21 17:27:08 -0300

    web api

--- Web/cadastro de animais/js/script.js
+++ Web/cadastro de animais/js/script.js
@@ -13,7 +13,11 @@ $(document).ready(
             animal.Idade = $('input[name="Idade"]').val();
             animal.Raca = $('input[name="Raca"]').val();
             animal.Coloracao = $('input[name="Coloracao"]').val();
+            //Limpa a tela assim que clicar no botão salvar (lembrando da função lá em baixo limparTela)
+            limparTela();
         });
+
+        
 
         $('button[name="Mostrar"]').click(function () {
             alert("Nome: " + animal.Nome
@@ -23,3 +27,11 @@ $(document).ready(
         });
     }
 );
+function limparTela() {
+    //obtemos todos os campos do tipo input
+    var meusInputs = $('input[type="text"]');
+    //loop para tds os campos
+    //limpamos o valor dos campos
+    meusInputs.val("");
+
+}
