41842d0e42a09ea9b43eea84a687937ca3fdc7f6
Fernando Coletti <nandocoletti@hotmail.com>
2019-11-05 17:40:31 -0200

    Ultima aula, infelizmente não deu tempo de acabar

---
+++ CadastroFinalWeb/CadastroFinalWeb/Models/CadastroFinalContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CadastroFinalWeb.Models
+{
+    public partial class CadastroFinalContext : DbContext
+    {
+        public CadastroFinalContext()
+        {
+        }
+
+        public CadastroFinalContext(DbContextOptions<CadastroFinalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.Property(e => e.EMail)
+                    .HasColumnName("E-Mail")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Login).HasMaxLength(30);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Senha).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
